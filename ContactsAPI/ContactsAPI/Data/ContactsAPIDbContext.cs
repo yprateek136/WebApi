d98845af55982cea34443b73aeedd6d8630a21f6
yprateek136 <yprateek195@gmail.com>
2022-12-08 20:07:24 +0530

    Dotnet Core Web Api

---
+++ ContactsAPI/ContactsAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ContactsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+namespace ContactsAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Contact> Contacts { get; set; } 
+    }
+}
